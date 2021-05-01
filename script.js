079ab5a6df7cb005ee4bca7bdfa28791c4a1cbbf
c-henry1 <78730639+c-henry1@users.noreply.github.com>
2021-05-01 21:05:57 +0100

    Day 2
    Work layout complete
    replace images.

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-elements-r');
+   
+    
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+    
+        //burg animation
+        burger.classList.toggle('toggle');
+    });
+
+    nav.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+        burger.classList.toggle('toggle');
+    });
+}
+
+navSlide();
 
