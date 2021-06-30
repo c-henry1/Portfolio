0b6dad1eadda0298e93bc43b448f4b84361260ab
c-henry1 <78730639+c-henry1@users.noreply.github.com>
2021-06-30 23:03:20 +0100

    To do..
    -Update images
    -Build project pages
    -Add Resume

--- script.js
+++ script.js
@@ -18,3 +18,17 @@ const navSlide = () => {
 
 navSlide();
 
+var slideIndex1 = 0;
+  carousel();
+  
+  function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    slideIndex1++;
+    if (slideIndex1 > x.length) {slideIndex1 = 1}
+    x[slideIndex1-1].style.display = "block";
+    setTimeout(carousel, 1000); // Change image every 2 seconds
+  }
